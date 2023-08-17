6928709480453263e619fd6ecb64d39f6ef02d10
Priyanka Patil <priyankapatil6372@gmail.com>
2023-08-17 15:21:32 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JobService } from 'src/service/service';
+import { FormBuilder } from '@angular/forms';
+import { Job } from 'src/model/job';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'my-app';
+  jobList: Job[] = [];
+
+  constructor(private formBuilder: FormBuilder, private jobService: JobService) { }
+
+  ngOnInit(): void {
+
+    // this.getAllJob();
+
+  }
+
+  getAllJob() {
+    this.jobService.getAllJob().subscribe(res => {
+      this.jobList = res;
+      console.log(res);
+    }, err => {
+      console.log("error while fetching data.")
+    });
+  }
 }
---
+++ src/service/service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Job } from 'src/model/job';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobService {
+
+  addJobURL: string;
+  getJobURL: string;
+  updateJobUrl: string;
+  deleteJobUrl: string;
+  getJobByUrl: string;
+  httpClient: any;
+  baseUrl: any;
+
+  constructor(private http: HttpClient) {
+
+    this.addJobURL = 'http://localhost:8080/create';
+    this.getJobURL = 'http://localhost:9100/get/all';
+    this.updateJobUrl = 'http://localhost:9100/asset/update';
+    this.deleteJobUrl = 'http://localhost:9100/asset/delete';
+    this.getJobByUrl = 'http://localhost:9100/asset/get';
+  }
+
+  addJob(job: Job): Observable<Job> {
+    return this.http.post<Job>(this.addJobURL, job);
+  }
+
+  getAllJob(): Observable<Job[]> {
+    return this.http.get<Job[]>(this.getJobURL);
+  }
+
+  /*updateEmployee(empId: Number, emp: Employee): Observable<Employee> {
+    return this.http.put<Employee>(this.updateEmpUrl, + '/' + empId, emp);
+  }
+  */
+  updateJob(id: Number, job: Job): Observable<Object> {
+    return this.http.put(`${this.updateJobUrl}/${id}`, job);
+  }
+
+  deleteJob(job: Job): Observable<Job> {
+    return this.http.delete<Job>(this.deleteJobUrl + '/' + job.id);
+  }
+
+  getJobById(job: Job): Observable<Job> {
+    return this.http.get<Job>(this.getJobByUrl + '/' + job.id);
+  }
+
+
+}
